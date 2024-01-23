ac73bec27cc24ac06f6e8905c9513bd4cbf4c783
Azar Taheri Tayebi <73050394+azar2020@users.noreply.github.com>
2024-01-23 18:10:14 -0500

    Update tabu.R

--- R/tabu.R
+++ R/tabu.R
@@ -353,6 +353,8 @@ cat("Final Matrix (sum of multiplied scores):\n")
 final_symmetric_matrix = final_matrix + t(final_matrix)
            cat("Final Symmetric Matrix:\n")
           print(final_symmetric_matrix)
+          # Plotting adjusted scores over iterations
+plot(1:iter, adjusted_scores, type = 'l', xlab = 'Iteration', ylab = 'Adjusted Score', main = 'Adjusted Scores Over Iterations')
   # Return the list of adjacency matrices along with the final network structure
   return(list(adjacency_matrices_list = adjacency_matrices_list, 
               best_scores_list = best_scores_list, 
