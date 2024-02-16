78a54406f73ad0bc430a8828b12d67c0fee12b4d
Azar Taheri Tayebi <73050394+azar2020@users.noreply.github.com>
2024-02-16 12:34:49 -0500

    Update tabu.R

--- R/tabu.R
+++ R/tabu.R
@@ -68,9 +68,9 @@ best_scores_list <- list()
    
   
     # Create an empty list to store adjacency matrices
-  adjacency_matrices_list <- list()
+    adjacency_matrices_list <- list()
     best_scores_all <- list()
- 
+    best_params_list <- list()  # New list to store the number of parameters for each iteration
     
   repeat {
 
@@ -290,6 +290,9 @@ best_scores_list <- list()
           
  if (iter %% 2 == 0) {
     best_scores_all[[length(best_scores_all) + 1]] <- sum(reference.score)
+       # Calculate and store the number of parameters for the current graph
+      params <- nparams.backend(x = start, data = x, debug = debug)
+      best_params_list[[length(best_params_list) + 1]] <- params
   }
      # Multiply weights by adjacency matrix
      weighted_matrix <- bestop$weights * amat
@@ -378,6 +381,7 @@ final_symmetric_matrix = final_matrix + t(final_matrix)
 # Return the list of adjacency matrices along with the final network structure
 return(list(adjacency_matrices_list = adjacency_matrices_list, 
             best_scores_list = best_scores_list, 
+             best_params_list = best_params_list,  # Include the list of parameters
             multiplied_scores = multiplied_scores,
             final_matrix = final_matrix,
             final_network = start,
