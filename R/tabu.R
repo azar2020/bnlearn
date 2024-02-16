29297bdaa692e9682ba5310de38d1011a8e85fd0
Azar Taheri Tayebi <73050394+azar2020@users.noreply.github.com>
2024-02-16 14:22:45 -0500

    Update tabu.R

--- R/tabu.R
+++ R/tabu.R
@@ -1,8 +1,6 @@
-
 # unified tabu search implementation (both optimized and by spec).
 tabu.search = function(x, start, whitelist, blacklist, score, extra.args,
     max.iter, maxp, optimized, tabu, debug = FALSE) {
-
   # cache nodes' labels.
   nodes = names(x)
   # cache the number of nodes.
@@ -26,25 +24,20 @@ tabu.search = function(x, start, whitelist, blacklist, score, extra.args,
   loss.iter = 0
   # keep track of the best score value.
   best.score = -Inf
-
   # set the reference score.
   reference.score = per.node.score(network = start, score = score,
                       targets = nodes, extra.args = extra.args, data = x)
-
   # convert the blacklist to an adjacency matrix for easy use.
   if (!is.null(blacklist))
     blmat = arcs2amat(blacklist, nodes)
   else
     blmat = matrix(0L, nrow = n.nodes, ncol = n.nodes)
-
   # convert the whitelist to an adjacency matrix for easy use.
   if (!is.null(whitelist))
     wlmat = arcs2amat(whitelist, nodes)
   else
     wlmat = matrix(0L, nrow = n.nodes, ncol = n.nodes)
-
   if (debug) {
-
     cat("----------------------------------------------------------------\n")
     cat("* starting from the following network:\n")
     print(start)
@@ -53,7 +46,6 @@ tabu.search = function(x, start, whitelist, blacklist, score, extra.args,
     if (!is.null(whitelist)) print(whitelist)
     cat("* blacklisted arcs are:\n")
     if (!is.null(blacklist)) print(blacklist)
-
     # set the metadata of the network; othewise the debugging output is
     # confusing and not nearly as informative.
     start$learning$algo = "tabu"
@@ -61,88 +53,64 @@ tabu.search = function(x, start, whitelist, blacklist, score, extra.args,
     start$learning$test = score
     start$learning$args = extra.args
     start$learning$optimized = optimized
-
   }#THEN
 TOTALSCORE <- 0
 best_scores_list <- list()
    
   
     # Create an empty list to store adjacency matrices
-    adjacency_matrices_list <- list()
+  adjacency_matrices_list <- list()
     best_scores_all <- list()
-    best_params_list <- list()  # New list to store the number of parameters for each iteration
-    total_params <- 0
+ 
+    
   repeat {
-
     current = as.integer((iter - 1) %% tabu)
-
     # keep the best network seen so far and its score value for the evaluation
     # of the stopping rule; but always create a "best network" in the first
     # iteration using the starting network.
     if ((sum(reference.score) == -Inf) && (best.score == -Inf) && (iter > 1)) {
-
       old.score = per.node.score(network = best.network, score = score,
                       targets = nodes, extra.args = extra.args, data = x)
-
       singular.old.nodes = (old.score == -Inf)
       singular.new.nodes = (reference.score == -Inf)
-
       if (all(singular.old.nodes == singular.new.nodes)) {
-
         # if the same nodes are singular/not singular in both networks,
         # disregard the singular nodes when comparing them.
         delta = robust.score.difference(
                   sum(reference.score[!singular.new.nodes]),
                   sum(old.score[!singular.old.nodes]))
-
       }#THEN
       else if (sum(singular.new.nodes) > sum(singular.old.nodes)) {
-
         # if there are more singular nodes in the new network than in the old
         # network, the old one is better.
         delta = -Inf
-
       }#THEN
       else if (sum(singular.new.nodes) < sum(singular.old.nodes)) {
-
         # if the new network ha fewer singular nodes than the old network, the
         # new one is better.
         delta = +Inf
-
       }#THEN
       else {
-
         delta = robust.score.difference(sum(reference.score), best.score)
-
       }#ELSE
-
       if (delta > 0) {
-
         best.network = start
         best.score = sum(reference.score)
-
       }#THEN
-
     }#ELSE
     else if ((robust.score.difference(sum(reference.score), best.score) > 0) ||
         (iter == 1)) {
-
       best.network = start
       best.score = sum(reference.score)
-
     }#THEN
-
     if (debug)
       cat("* iteration", iter, "using element", current, "of the tabu list.\n")
-
     # build the adjacency matrix of the current network structure.
     amat = arcs2amat(start$arcs, nodes)
       # Append the current adjacency matrix to the list
     adjacency_matrices_list[[iter]] <- amat
-
     # compute the number of parents of each node.
     nparents = colSums(amat)
-
     # add the hash of the network into the tabu list for future reference.
     # (BEWARE: in place modification of tabu.list!)
     .Call(call_tabu_hash,
@@ -150,7 +118,6 @@ best_scores_list <- list()
           nodes = nodes,
           tabu.list = tabu.list,
           current = current)
-
     # set up the score cache (BEWARE: in place modification!).
     .Call(call_score_cache_fill,
           nodes = nodes,
@@ -168,13 +135,11 @@ best_scores_list <- list()
           debug = debug)
           
    
-
     # select which arcs should be tested for inclusion in the graph (hybrid
     # learning algorithms should hook the restrict phase here).
     to.be.added = arcs.to.be.added(amat = amat, nodes = nodes,
                     blacklist = blmat, whitelist = NULL, nparents = nparents,
                     maxp = maxp, arcs = FALSE)
-
     # get the best arc addition/removal/reversal.
     bestop = .Call(call_tabu_step,
                    amat = amat,
@@ -191,54 +156,36 @@ best_scores_list <- list()
                    maxp = maxp,
                    debug = debug)
 #AZARRRRRRRRRRRRRRR
-
     
           cur_score <- bestop$score
             cat("* sum(reference.score) in iteration", iter, ":", sum(reference.score), "\n")
-
-
-
     
            
             #cat("Weight in iteration", iter , ":", best.score/TOTALSCORE, "\n")
-
     # the value FALSE is the canary value in bestop$op meaning "no operation
     # improved the network score"; reconsider prevously discarded solutions
     # and find the one that causes the minimum decrease in the network score.
     if (bestop$op == FALSE) {
-
       if (loss.iter >= max.loss.iter) {
-
         # reset the return value to the best network ever found.
         start = best.network
-
         if (debug) {
-
           cat("----------------------------------------------------------------\n")
           cat("* maximum number of iterations without improvements reached, stopping.\n")
           cat("* best network ever seen is:\n")
           print(best.network)
-
         }#THEN
-
         break
-
       }#THEN
       else {
-
         # increase the counter of the iterations without improvements.
         loss.iter = loss.iter + 1
-
       }#ELSE
-
       if (debug) {
-
         cat("----------------------------------------------------------------\n")
         cat("* network score did not increase (for", loss.iter,
               "times), looking for a minimal decrease :\n")
-
       }#THEN
-
       bestop = .Call(call_tabu_step,
                      amat = amat,
                      nodes = nodes,
@@ -253,47 +200,32 @@ best_scores_list <- list()
                      nparents = nparents,
                      maxp = maxp,
                      debug = debug)
-
       # it might be that there are no more legal operations.
       if (bestop$op == FALSE) {
-
        if (debug) {
-
          cat("----------------------------------------------------------------\n")
          cat("* no more possible operations.\n")
          cat("@ stopping at iteration", iter, ".\n")
-
        }#THEN
-
        # reset the return value to the best network ever found.
        if (loss.iter > 0)
          start = best.network
-
        break
-
      }#THEN
-
     }#THEN
     else {
-
       if (robust.score.difference(sum(reference.score), best.score) > 0)
         loss.iter = 0
-
     }#ELSE
-
     # update the network structure.
     start = arc.operations(start, from = bestop$from, to = bestop$to,
               op = bestop$op, check.cycles = FALSE, check.illegal = FALSE,
               update = TRUE, debug = FALSE)
 #AZARRRRRRRRRRRRRRRRRRRRRRRRRRRR
        
-          
+
  if (iter %% 2 == 0) {
     best_scores_all[[length(best_scores_all) + 1]] <- sum(reference.score)
-       # Calculate and store the number of parameters for the current graph
-      params <- nparams.backend(x = start, data = x, debug = debug)
-      best_params_list[[length(best_params_list) + 1]] <- params
-      total_params <- total_params + params
   }
      # Multiply weights by adjacency matrix
      weighted_matrix <- bestop$weights * amat
@@ -301,19 +233,17 @@ best_scores_list <- list()
     #cat("* Weighted Matrix in iteration", iter, ":\n")
     #print(weighted_matrix)
     best_scores_list <- c(best_scores_list, sum(reference.score))
-               TOTALSCORE <- TOTALSCORE + sum(reference.score)
+    
+           TOTALSCORE <- TOTALSCORE + sum(reference.score)
     # set the nodes whose cached score deltas are to be updated.
     if (bestop$op == "reverse")
       updated = which(nodes %in% c(bestop$from, bestop$to)) - 1L
     else
       updated = which(nodes %in% bestop$to) - 1L
-
     if (debug) {
-
       # update the test counter of the network; very useful to check how many
       # score comparison has been done up to now.
       start$learning$ntests = test.counter()
-
       cat("----------------------------------------------------------------\n")
       cat("* best operation was: ")
       if (bestop$op == "set")
@@ -324,30 +254,22 @@ best_scores_list <- list()
         cat("reversing", bestop$from, "->", bestop$to, ".\n")
       cat("* current network is :\n")
       print(start)
-
        
       #cat("* current score:", sum(reference.score), "\n")
       cat(sprintf("* best score up to now: %s (delta: %s)\n",
         format(best.score),
         format(robust.score.difference(sum(reference.score), best.score))))
-
     }#THEN
-
     # check the current iteration index against max.iter.
     if (iter >= max.iter) {
-
       if (debug)
         cat("@ stopping at iteration", max.iter, ".\n")
-
       # reset the return value to the best network ever found.
       if (loss.iter > 0)
         start = best.network
-
       break
-
     }#THEN
     else iter = iter + 1
-
   }#REPEAT
   cat("Total Summation of Scores after", iter, "iterations:", TOTALSCORE, "\n")
   adjusted_scores <- as.numeric(unlist(best_scores_list)) / TOTALSCORE
@@ -377,25 +299,18 @@ final_symmetric_matrix = final_matrix + t(final_matrix)
           print(final_symmetric_matrix)
   # Plot the final graph
  final_graph <- graphviz.plot(start) 
-
 # Return the list of adjacency matrices along with the final network structure
 return(list(adjacency_matrices_list = adjacency_matrices_list, 
             best_scores_list = best_scores_list, 
-             best_params_list = best_params_list,  # Include the list of parameters
             multiplied_scores = multiplied_scores,
             final_matrix = final_matrix,
             final_network = start,
-            final_graph = final_graph,
-             total_params = total_params
+            final_graph = final_graph
            ))
-
- cat("Total Number of Parameters after", iter, "iterations:", total_params, "\n")
  # return(list(adjacency_matrices_list = adjacency_matrices_list, final_network = start))
 # Save best scores list to CSV
 best_scores_df <- data.frame(iteration = seq_along(best_scores_list), score = best_scores_list)
 write.csv(best_scores_df, file = "C:/Azar_Drive/relationships-between-variables1/01_preprocessing/best_scores.csv", row.names = FALSE)
 cat("Best scores list saved to 'best_scores.csv'\n")
   #return(start)
-
 }#TABU.SEARCH
-
